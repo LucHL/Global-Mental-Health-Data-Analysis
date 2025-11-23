dfb91d4666763559342938ce6f842f812634b828
Luc <luc.helmlinger@epitech.eu>
2025-11-23 12:28:30 +0900

    feat readme.md and sheband & fix '/' char in read_csv

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mental health project
+
+## How to start on Linux :
+
+```sh
+chmod u+x mental_health_project.py
+./mental_health_project.py
+```
+
+## How to start on Window :
+
+Install VsCode or another code editor.
+On the editor, click on the run button.
--- mental_health_project.py
+++ mental_health_project.py
@@ -1,14 +1,13 @@
+#!/usr/bin/python3
 import pandas as pd
 
 data = pd.read_csv("archive/Mental Health Dataset.csv")
-data = data.drop(['Growing_Stress', 'Changes_Habits', 'Mood_Swings', 'Coping_Struggles',
+data = data.drop(['Growing_Stress', 'Changes_Habits', 'Mood_Swings', 'Coping_Struggles', 
              'mental_health_interview', 'care_options'], axis=1)
-
 
 for name, group in data.groupby('Country'):
     print(name)
     print(group)
-
 
 
 # print(data.isnull().sum(), "\n") #check whether there is NA or not
